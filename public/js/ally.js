39b36307217ab0183b09988ede50a5caec193179
Maroko Gideon <marokogideon@gmail.com>
2025-11-13 20:49:37 +0300

    apply styles and tokens

--- public/js/ally.js
+++ public/js/ally.js
@@ -1,12 +1,12 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const skipLink = document.querySelector('.skip-link');
-  const mainContent = document.getElementById('main');
+// document.addEventListener('DOMContentLoaded', () => {
+//   const skipLink = document.querySelector('.skip-link');
+//   const mainContent = document.getElementById('main');
 
-  if (skipLink && mainContent) {
-    skipLink.addEventListener('click', (e) => {
-      e.preventDefault();
-      mainContent.setAttribute('tabindex', '-1');
-      mainContent.focus();
-    });
-  }
-});
+//   if (skipLink && mainContent) {
+//     skipLink.addEventListener('click', (e) => {
+//       e.preventDefault();
+//       mainContent.setAttribute('tabindex', '-1');
+//       mainContent.focus();
+//     });
+//   }
+// });
